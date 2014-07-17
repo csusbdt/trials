fe6ad563e27d5c0ae596474e0e58652da599832c
David Turner <csusbdt@gmail.com>
2014-07-16 21:01:35 -0700

    X

--- game_sound.lua
+++ game_sound.lua
@@ -1,16 +1,15 @@
 game_sound = {}
 
-local item_font = fonts.create("fonts/CaviarDreams.ttf", 22)
-local menu_font = fonts.create("fonts/CaviarDreams.ttf", 24)
+--local item_font = fonts.create("fonts/CaviarDreams.ttf", 22)
+--local menu_font = fonts.create("fonts/CaviarDreams.ttf", 24)
 
 local exit_button 
 
 function game_sound.draw()
-
 	-- Display overlay background.
 
-	local overlay_w = 500
-	local overlay_h = 250
+	local overlay_w = 1000
+	local overlay_h = 700
 	local overlay_x = (design_width  - overlay_w) / 2
 	local overlay_y = (design_height - overlay_h) / 2
 
@@ -19,15 +18,16 @@ function game_sound.draw()
         fill_rect(overlay_x, overlay_y, overlay_w, overlay_h)
         blendmode_none()
 
-	local x = overlay_x + 30
-	local y = overlay_y + 30
+	local x = overlay_x + 70
+	local y = overlay_y + 100
 
 	local texture
-	local dy = 30
+	local dy = 100
 
 	texture = ui.overlay_text_font:text("Sound and Music Level Adjustment", ui.overlay_text_color)
+	x = overlay_x + (overlay_w - texture.w) / 2
 	texture:draw(x, y)
-	y = overlay_y + overlay_h - 40
+	y = overlay_y + overlay_h - 200
 	texture = ui.overlay_button_font:text("EXIT", ui.overlay_button_color)
 	exit_button = buttons.create_from_texture(texture, x, y)
 
