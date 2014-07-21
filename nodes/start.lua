60d78498a492a9f427272277b03a07cc025710a9
David Turner <csusbdt@gmail.com>
2014-07-21 14:12:08 -0700

    X

--- nodes/start.lua
+++ nodes/start.lua
@@ -1,6 +1,6 @@
 ui.date  = "June 13 morning"
 ui.bg    = 'bg/cecilias_room.jpg'
-ui.lg    = 'lg/ghost_smile.png'
+ui.lg    = 'lg/ghost_shock.png'
 ui.sm    = 'sm/MC.png'
 ui.n     = 'CECILIA'
 ui.d     = { "I'm Cecilia.", "The hero of this story." }
@@ -10,7 +10,7 @@ log()
 sequence({
 	{ n = 'DAMIEN', d = { "I'm Damien.", "The villain." } },
 	{ n = 'SYSTEM', d = { "Portrait test.", "sm = 400 x 600", "lg = 800 x 1300" }, lg = '', sm = '' }, 
-	{ d = { "I am every where." }, sm = 'sm/MC.png', lg = 'lg/DameAnnoy.png' },
+	{ d = { "I am every where." }, sm = 'sm/MC_shock.png', lg = 'lg/ghost_shock.png' },
 	{ n = 'CECILIA', d = { "OK." } },
 	{ d = { "Bye." }, next = next_node_function('nodes/library/entrance.lua') }
 })
