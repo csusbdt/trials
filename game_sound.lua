88589b92e38880a26fb32ad5102e8c4311ce8a2d
David Turner <csusbdt@gmail.com>
2014-07-24 23:40:42 -0700

    X

--- game_sound.lua
+++ game_sound.lua
@@ -4,6 +4,8 @@ game_sound = {}
 --local menu_font = fonts.create("fonts/CaviarDreams.ttf", 24)
 
 local exit_button 
+local down_button 
+local up_button 
 
 function game_sound.draw()
 	-- Display overlay background.
@@ -22,21 +24,50 @@ function game_sound.draw()
 	local y = overlay_y + 100
 
 	local texture
-	local dy = 100
 
-	texture = ui.overlay_text_font:text("Sound and Music Level Adjustment", ui.overlay_text_color)
+	texture = ui.overlay_text_font:text("Volume Adjustment", ui.overlay_text_color)
 	x = overlay_x + (overlay_w - texture.w) / 2
 	texture:draw(x, y)
-	y = overlay_y + overlay_h - 200
-	texture = ui.overlay_button_font:text("EXIT", ui.overlay_button_color)
-	exit_button = buttons.create_from_texture(texture, x, y)
 
+	local vol = get_volume()
+	texture = ui.overlay_text_font:text('X', ui.overlay_text_color)
+	y = y + 100
+	x = overlay_x + overlay_w / 2 - 150 + vol * 300
+	texture:draw(x, y)
+
+	texture = ui.overlay_button_font:text('DOWN', ui.overlay_text_color)
+	y = y + 100
+	x = overlay_x + (overlay_w - texture.w) / 3
+	down_button = buttons.create_from_texture(texture, x, y)
+	down_button:draw()
+
+	texture = ui.overlay_button_font:text('UP', ui.overlay_text_color)
+	x = overlay_x + 2 * (overlay_w - texture.w) / 3
+	up_button = buttons.create_from_texture(texture, x, y)
+	up_button:draw()
+
+	texture = ui.overlay_button_font:text("EXIT", ui.overlay_button_color)
+	y = overlay_y + overlay_h - 200
+	x = overlay_x + (overlay_w - texture.w) / 2
+	exit_button = buttons.create_from_texture(texture, x, y)
 	exit_button : draw()
 end
 
 function game_sound.on_touch(x, y)
 	if exit_button:contains(x, y) then 
 		ui.overlay = 'none'
+		return true
+	elseif down_button:contains(x, y) then
+		local vol = get_volume()
+		vol = vol - .05
+		if vol < 0 then vol = 0 end
+		set_volume(vol)
+		return true
+	elseif up_button:contains(x, y) then
+		local vol = get_volume()
+		vol = vol + .05
+		if vol > 1 then vol = 1 end
+		set_volume(vol)
 		return true
 	end
 	return false
