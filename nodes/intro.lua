f6d0fba53d90a25f93b6f3f0b134d4d37196a058
David Turner <csusbdt@gmail.com>
2014-07-27 19:11:27 -0700

    X

--- nodes/intro.lua
+++ nodes/intro.lua
@@ -7,7 +7,7 @@ add({ sm = sm.mc, m = mu.friendly, d = { "I hear a girl crying." } })
 
 add({ d = { "I hesitantly flip on the lights." } })
 
-add({ n = 'CECILIA', bg = bg.cecilia_room_night, sm = sm.mc_shock.png, d = { "EEEEEEEK!  A GHOST!" } })
+add({ n = 'CECILIA', bg = bg.cecilia_room_night, sm = sm.mc_shock, d = { "EEEEEEEK!  A GHOST!" } })
 
 add({ n = 'GHOST', lg = lg.ghost_smile, m = '', d = { "..." } })
 
